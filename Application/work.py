4e7488af69006a7efec7015363fa8e2641f2c0c5
sinturkgozde <gozde.sinturk@bilgiedu.net>
2016-07-01 10:05:08 +0300

    Multiprocessing for findWiki method

--- Application/work.py
+++ Application/work.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+ # -*- coding: utf-8 -*-
 import helper
 from gensim.models import Word2Vec
 from nltk.stem.porter import *
@@ -14,9 +16,11 @@ from functools import partial
 import wikipedia
 from dateutil.parser import _timelex, parser
 from nltk.util import ngrams
-
-
-
+import urllib
+import simplejson
+import time
+import cProfile
+import ptest
 
 ###################################################################
 '''
@@ -138,17 +142,24 @@ def findingPossibleName(posed_data):
 	
 	wiki, wikipedia uzerinden "name" olarak etiketlendirdigimiz A Clockwork Orange filminin sayfasina gider, ve butun icerigi ceker.
 '''
+import datetime
+
+def start_process2():
+	print multiprocessing.current_process().name
 def findWiki(nlist):
 	nameList = []
 	for i in nlist:
 		try:
-			for element in wikipedia.search(i):
-				if wikipedia.page(i).title == wikipedia.page(element).title:
-					nameList.append(i)
+			main_title = wikipedia.page(i).title		
+			main_search = wikipedia.search(i)
+			name = threadWiki(main_search, main_title)
+			for el in name:
+				if el != None:
+					nameList.append(el)
 		except:
 			pass
 	name = max(nameList, key=len)
-	wiki =wikipedia.page(name).content.replace("\n","").replace("=","").replace('\\',"").split(".")	
+	wiki =wikipedia.page(name).content.replace("\n","").replace("=","").replace('\\',"").split(".")
 	org_name = wikipedia.page(name).title.decode('iso-8859-9').encode('utf-8',',ignore')	
 	if "(" in org_name:
 		org_name_index= org_name[0:org_name.index(" (")]
@@ -156,6 +167,28 @@ def findWiki(nlist):
 		org_name_index = org_name
 	
 	return (wiki,org_name_index) 
+
+
+def threadWiki(nlist,main_title):
+	func = partial(wikipedia_search, main_title)
+	pool_size =multiprocessing.cpu_count() * 2
+	#pool_size = multiprocessing.Pool(len(array))
+	pool =multiprocessing.Pool(processes=pool_size,initializer=start_process2 ) 
+	results = pool.map(func, nlist)
+
+	pool.close()
+	pool.join()
+	
+	return results
+
+def wikipedia_search(main_title,element):
+	try:
+		if main_title == wikipedia.page(element).title:
+			return main_title
+	except:
+		pass
+	
+
 
 ############################################################################
 
@@ -175,12 +208,15 @@ def searchContext(sentence):
 	posed_data = possingData(sentence)
 	
 	verb = [element for element in posed_data if  'VB' in element[1]] 
-	if len(verb) <= 1 and verb[0][0] in stop_words:
+	if len(verb) <= 1 and str(verb[0][0]) in stop_words:
 		verbs = verb[0][0]
 		
 	else:
-		verbs= [element for element in verb if element[0] not in stop_words]
-		verbs =  stemmer2.stem(verb[0][0])        
+		for element in verb:
+			if str(element[0]) not in stop_words:
+				verbs =  stemmer2.stem(element[0])
+		    
+		
 	
 	nlist = findingPossibleName(posed_data)
 	
@@ -216,12 +252,11 @@ def extractSimilarSentences(noun,verb,data,question):
 		sim_hyp_verb= helper.merge_Hyp_Sys(verb)
 		sim_hyp_verb= filter(lambda x: x in model.vocab, sim_hyp_verb)
 		similarity_checking= filter(lambda x:model.similarity(x,verb) > 0.35, sim_hyp_verb)
-		sentence_no = 0
 		
 		for sentence in data:
 			for word in nltk.word_tokenize(sentence):
 				if stemmer.stem(word) in map(lambda x:stemmer.stem(x),similarity_checking) :
-					result_array.append(str(sentence_no) +" "+sentence)
+					result_array.append(sentence)
 		 
 		return result_array					
 	elif verb in stop_words:
@@ -371,6 +406,31 @@ def extractCorrectAnswers(result):
 	
 	return potential
 	
+	
+	
+######################################################################
+
+googleGeocodeUrl = 'http://maps.googleapis.com/maps/api/geocode/json?'
+
+def get_location(noun):
+    noun = noun.encode('utf-8')
+    params = {
+        'address': noun,
+        'sensor': "false"
+    }
+    url = googleGeocodeUrl + urllib.urlencode(params)
+    json_response = urllib.urlopen(url)
+    response = simplejson.loads(json_response.read())
+    if response['results']:
+        location = response['results'][0]['geometry']['location']
+        latitude, longitude = location['lat'], location['lng']
+    else:
+        latitude, longitude = None, None
+
+    return latitude, longitude
+
+
+	
 #########################################################################
 
 	
@@ -393,63 +453,94 @@ def extractCorrectAnswers(result):
 
 
 
-'''               
+'''    
+
+def returnNameOrLoc(noun,verb,data,tag,result,flag):
+	resultArray = []
+	allSentences = []
+	if verb in stop_words:
+		try:
+			pg = wikipedia.page(noun).summary
+			img = findImage(noun)
+			return [[pg],[img],tag,flag]
+		except:
+			pass
+	lastResult=[]
+	extracted=[]
+	
+	for i in range(0,len(result)):
+		if ","  in result[i]:
+			result[i]  = result[i].replace("," , " ,")
+		
+		recieved_data_from_server = clientSocket.analyzeSentence(result[i])
+		checkInfo = searchAnswer.checkIfTag(tag,recieved_data_from_server)
+						
+			
+		if checkInfo[0]:
+			splitted_sentence = recieved_data_from_server.split(" ")
+			extracted = extractRelations(splitted_sentence,checkInfo[2],resultArray)
+			lastResult.append(extracted)
+		
+	count_result =countSuccessful(extracted,noun)
+		
+		
+		
+	correct_answer = extractCorrectAnswers(count_result)
+	
+	for i in result:
+		for elem in correct_answer:
+			if elem in i and i not in allSentences:
+				allSentences.append(i)
+	
+	for i in range(0,len(correct_answer)):
+		correct_answer[i] = correct_answer[i][1:]
+	
+		
+	img = []
+	for element in correct_answer:
+		element_appended = findImage(element)
+		if element_appended != None:
+			img.append(element_appended)
+	
+	print allSentences
+	print img
+		
+	return [allSentences, img, tag[0], flag]           
 
 def extractWithStanfordNer(question):
+	
 	resultArray = []
 	text =  searchContext(question)
-	result = extractSimilarSentences(text[1],text[2],text[0],question)
-	
-	
-	
+	result = extractSimilarSentences(text[1],text[2],text[0],question)	
+	allSentences =[]
+	location_flag = False
 	classifier_tag = Qc.classify_question(classifier,[question])
 	
 	if classifier_tag[0] == "NUM":
 		
-		return [extractDates(result), ""]
+		return [extractDates(result), "", classifier_tag[0],location_flag]
 	
-	
-	elif  classifier_tag[0] == "HUM" or classifier_tag[0] == "LOC":
-		lastResult = []
-		a = []
-		for i in range(0,len(result)):
-			if ","  in result[i]:
-				result[i]  = result[i].replace("," , " ,")
-				
-		
-			recieved_data_from_server = clientSocket.analyzeSentence(result[i])
-			checkInfo = searchAnswer.checkIfTag(classifier_tag,recieved_data_from_server)
-			
-			
-			if checkInfo[0]:
-				splitted_sentence = recieved_data_from_server.split(" ")
-				a = extractRelations(splitted_sentence,checkInfo[2],resultArray)
-				lastResult.append(a)
-		count_result =countSuccessful(a,text[1])
-		
-		
-		
-		correct_answer = extractCorrectAnswers(count_result)
-		for i in range(0,len(correct_answer)):
-			correct_answer[i] = correct_answer[i][1:]
-		
-		
-		img = []
-		for element in correct_answer:
-			element_appended = findImage(element)
-			if element_appended != None:
-				img.append(element_appended)
-
-		
-		return [correct_answer, img]
+	elif  classifier_tag[0] == "HUM":
+		return returnNameOrLoc(text[1],text[2],text[0],classifier_tag,result,location_flag)
 	elif classifier_tag[0] == "DESC" or classifier_tag[0] == "ENTY":
 		text =text[1]
 		img=findImage(text)
 		mysummary = wikipedia.summary(text)
 		
-		return [[mysummary],[img]]		
+		return [[mysummary],[img],classifier_tag[0],location_flag]		
+	
+	
+	elif classifier_tag[0] == "LOC":
+		recieved_data_from_server = clientSocket.analyzeSentence(question)
+		if "/LOCATION" in recieved_data_from_server:
+			img=findImage(text[1])
+			location_flag = True
+			return [[get_location(text[1])],[img],classifier_tag[0],location_flag]
+		else:
+			return returnNameOrLoc(text[1],text[2],text[0],classifier_tag,result,location_flag)
+	
 	else:
-		return 	[["This is  not a proper question"], ""]
+		return 	[["This is  not a proper question"], "",classifier_tag[0],location_flag]
 
 
 
@@ -465,13 +556,15 @@ def extractWithStanfordNer(question):
 
 '''
 def findImage(name):
-        page = wikipedia.page(name).images
+    try:
+       	page = wikipedia.page(name).images
         splittedName = name.split(" ")
         for index in range(0,len(page)):
                 for element in splittedName:
                         if element in page[index]:
-							
 							return page[index]
+    except:
+        pass
 
 #################################################################################################
 
